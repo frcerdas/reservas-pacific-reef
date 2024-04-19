2dacd6a8bf6539bf4bf6bfb838c0752336f0551d
Alexis <a.varela@duocuc.cl>
2024-04-18 23:43:41 -0400

    avance back end

--- HotelAPP/HotelMVP/urls.py
+++ HotelAPP/HotelMVP/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import ReservasLista, HabitacionesLista
+from .views import ReservasLista, HabitacionesLista, ReservasVista
 app_name='HotelMVP'
 
 urlpatterns = [
     path('habitaciones_list/', HabitacionesLista.as_view(), name='HabitacionesLista'),
     path('reservas_list/', ReservasLista.as_view(), name='ReservasLista'),
+    path('disponibilidad_form/',ReservasVista.as_view(), name='ReservasVista'),
 ]
--- HotelAPP/HotelMVP/views.py
+++ HotelAPP/HotelMVP/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 from .models import Habitaciones, Reservas
-from django.views.generic import ListView
+from django.views.generic import ListView, FormView
+from .forms import DisponibilidadForm
+from HotelMVP.reservas_funciones.disponibilidad import revisar_disponibilidad
 
 # Create your views here.
 class HabitacionesLista(ListView):
@@ -8,3 +10,30 @@ class HabitacionesLista(ListView):
 
 class ReservasLista(ListView):
     model = Reservas
+
+class ReservasVista(FormView):
+    form_class = DisponibilidadForm
+    template_name = 'disponibilidad_form.html'
+
+    def form_valid(self, form): 
+        data = form.cleaned_data
+        habitaciones_list = Habitaciones.objects.filter(categoria=data['habitaciones_categoria'])
+        disponibilidad_habitaciones = []
+        
+        for habitacion in habitaciones_list:
+            if revisar_disponibilidad(habitacion, data['fecha_inicio'], data['fecha_fin']):
+                disponibilidad_habitaciones.append(habitacion)
+
+        if len(disponibilidad_habitaciones) > 0:
+            habitacion = disponibilidad_habitaciones[0]
+            reservas = Reservas.objects.create(
+                user=self.request.user,
+                habitacion=habitacion,
+                fecha_inicio=data['fecha_inicio'],
+                fecha_fin=data['fecha_fin']
+            )
+            reservas.save()
+            return HttpResponse(reservas)
+        else:
+            return HttpResponse('Todas las habitaciones de esta categoria están reservadas!! Inténte otra categoria')
+
