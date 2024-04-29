463c97c3704eab639d51f60a255b8ff25c75c2a2
Alexis <a.varela@duocuc.cl>
2024-04-29 02:00:51 -0400

    1

--- HotelAPP/HotelMVP/reservas_funciones/get_habitacion_categoria_formato_legible.py
+++ HotelAPP/HotelMVP/reservas_funciones/get_habitacion_categoria_formato_legible.py
@@ -4,13 +4,3 @@ def get_habitacion_categoria_formato_legible(categoria):
     habitacion = Habitaciones.objects.all()[0]
     habitacion_categoria = dict(Habitaciones.HAB_CATEGORIA).get(categoria, None)
     return habitacion_categoria
-
-#    #habitaciones_list = Habitaciones.objects.filter(categoria=categoria)
-
-    #if len(habitaciones_list) > 0:
-    #    habitacion = habitaciones_list [0]
-    #    capacidad = habitacion.capacidad
-   #     habitacion_categoria = dict(habitacion.HAB_CATEGORIA).get(habitacion.categoria, None)
-    #    return habitacion_categoria
-   # else:
-    #    return None
